3bf8adc300d11e6901be80ec9632d2d0a8b0a0da
robert baxter <baxterrp@gmail.com>
2018-10-31 19:46:49 -0400

    using html.dropdownlist for select elements

---
+++ CTRL_Core.Web/Models/TestModel.cs
@@ -0,0 +1,13 @@
+﻿using CTRL_Core.Backbone.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CTRL_Core.Web.Models
+{
+    public class TestModel
+    {
+        public State SelectedState { get; set; }
+    }
+}
